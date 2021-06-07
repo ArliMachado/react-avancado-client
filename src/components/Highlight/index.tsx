3d9c1b61df01528ab78fbd4e51991d2078d9edb8
Arli Machado <arli.machado@gmail.com>
2021-06-07 11:31:49 -0300

    Definindo componente Highlight

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,38 @@
+import Button from 'components/Button';
+import * as S from './styles';
+
+export type AlignmentProps = 'right' | 'left';
+
+export type HighlightProps = {
+  title: string;
+  subtitle: string;
+  backgroundImage: string;
+  floatImage?: string;
+  buttonLabel: string;
+  buttonLink: string;
+  alignment?: AlignmentProps;
+};
+
+const Highlight = ({
+  title,
+  subtitle,
+  backgroundImage,
+  floatImage,
+  buttonLabel,
+  buttonLink,
+  alignment = 'right',
+}: HighlightProps) => (
+  <S.Wrapper alignment={alignment} backgroundImage={backgroundImage}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+    <S.Content>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Content>
+  </S.Wrapper>
+);
+
+export default Highlight;
