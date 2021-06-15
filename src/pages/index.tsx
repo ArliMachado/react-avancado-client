4415ab600a4d5f56ba4090fe6b9e09fc630b3e53
Arli Machado <arli.machado@gmail.com>
2021-06-14 21:33:42 -0300

    Definindo a home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import Main from 'components/Main';
+import Home, { HomeTemplateProps } from 'templates/Home';
 
-export default function Home() {
-  return <Main />;
+import bannersMock from 'components/BannerSlider/mock';
+import gamesMock from 'components/GameCardSlider/mock';
+import highlightsMock from 'components/Highlight/mock';
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />;
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightsMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highlightsMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: highlightsMock,
+    },
+  };
 }
