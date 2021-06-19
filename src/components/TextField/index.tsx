050f413d97687cec384b1b1a81b4ad398236e7e5
Arli Machado <arli.machado@gmail.com>
2021-06-19 15:47:11 -0300

    Definindo textField

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,54 @@
+import { useState, InputHTMLAttributes } from 'react';
+
+import * as S from './styles';
+
+export type IconPositionProps = 'left' | 'right';
+
+export type TextFieldProps = {
+  onInput?: (value: string) => void;
+  label?: string;
+  labelFor?: string;
+  initialValue?: string;
+  icon?: React.ReactNode;
+  iconPosition?: IconPositionProps;
+  disabled?: boolean;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const TextField = ({
+  label,
+  labelFor = '',
+  initialValue = '',
+  icon,
+  iconPosition = 'left',
+  onInput,
+  disabled = false,
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value;
+    setValue(newValue);
+
+    !!onInput && onInput(newValue);
+  };
+
+  return (
+    <S.Wrapper disabled={disabled}>
+      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      <S.InputWrapper>
+        {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
+        <S.Input
+          type="text"
+          onChange={onChange}
+          value={value}
+          {...props}
+          iconPosition={iconPosition}
+          disabled={disabled}
+        />
+      </S.InputWrapper>
+    </S.Wrapper>
+  );
+};
+
+export default TextField;
---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,124 @@
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { AccountCircle } from '@styled-icons/material-outlined/AccountCircle';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import TextField from '.';
+
+describe('<TextField />', () => {
+  it('Renders with Label', () => {
+    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />);
+
+    expect(screen.getByLabelText('Label')).toBeInTheDocument();
+  });
+
+  it('Renders without Label', () => {
+    renderWithTheme(<TextField />);
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument();
+  });
+
+  it('Renders with placeholder', () => {
+    renderWithTheme(<TextField placeholder="hey you" />);
+
+    expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument();
+  });
+
+  it('Renders with icon', () => {
+    renderWithTheme(<TextField icon={<AccountCircle data-testid="icon" />} />);
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument();
+  });
+
+  it('Renders with Icon on the right side', () => {
+    renderWithTheme(
+      <TextField
+        icon={<AccountCircle data-testid="icon" />}
+        iconPosition="right"
+      />,
+    );
+
+    expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 });
+  });
+
+  it('Does not changes its value when disabled', async () => {
+    const onInput = jest.fn();
+
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="Label"
+        labelFor="Field"
+        id="Field"
+        disabled
+      />,
+    );
+
+    const input = screen.getByRole('textbox');
+
+    expect(input).toBeDisabled();
+    expect(input).toHaveStyle({
+      color: '#8F8F8F',
+    });
+
+    expect(screen.getByLabelText('Label')).toHaveStyle({
+      color: '#8F8F8F',
+    });
+
+    const text = 'This is my new text';
+    userEvent.type(input, text);
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text);
+    });
+    expect(onInput).not.toHaveBeenCalled();
+  });
+
+  it('Is not accessible by tab when disabled', () => {
+    renderWithTheme(
+      <TextField label="Label" labelFor="TextField" id="TextField" disabled />,
+    );
+
+    const input = screen.getByLabelText('Label');
+
+    expect(document.body).toHaveFocus();
+
+    userEvent.tab();
+    expect(input).not.toHaveFocus();
+  });
+
+  it('Changes its value when typing', async () => {
+    const onInput = jest.fn();
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+      />,
+    );
+
+    const input = screen.getByRole('textbox');
+    const text = 'This is my new text';
+    userEvent.type(input, text);
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text);
+      expect(onInput).toHaveBeenCalledTimes(text.length);
+    });
+    expect(onInput).toHaveBeenCalledWith(text);
+  });
+
+  it('Is accessible by tab', () => {
+    renderWithTheme(
+      <TextField label="TextField" labelFor="TextField" id="TextField" />,
+    );
+
+    const input = screen.getByLabelText('TextField');
+    expect(document.body).toHaveFocus();
+
+    userEvent.tab();
+    expect(input).toHaveFocus();
+  });
+});
