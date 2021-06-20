c8eb9db73d34647a379646eac23a9768424988f4
Arli Machado <arli.machado@gmail.com>
2021-06-20 11:46:09 -0300

    Definindo estrutura inicial do template de Auth

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,40 @@
+import { screen, waitFor } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Auth from '.';
+
+describe('<Auth />', () => {
+  it('should render all components and children', async () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>,
+    );
+
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2);
+
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i,
+      }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', {
+        name: /WON is the best and most complete gaming platform./i,
+      }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText('Won Games 2020 © Todos os Direitos Reservados'),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Auth title/i,
+      }),
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+});
